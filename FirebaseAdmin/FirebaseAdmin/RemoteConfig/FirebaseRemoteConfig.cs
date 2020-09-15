0ec21256fae4f56895c5038a4bc41cb1dbb1a926
Kevin Goos <kevingoos@outboxit.be>
2020-09-15 22:29:31 +0200

    Working on classes

--- FirebaseAdmin/FirebaseAdmin/RemoteConfig/FirebaseRemoteConfig.cs
+++ FirebaseAdmin/FirebaseAdmin/RemoteConfig/FirebaseRemoteConfig.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FirebaseAdmin.Messaging;
 
 namespace FirebaseAdmin.RemoteConfig
 {
@@ -45,6 +48,209 @@ namespace FirebaseAdmin.RemoteConfig
             }
 
             return app.GetOrInit(nameof(FirebaseRemoteConfig), () => new FirebaseRemoteConfig(app));
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> GetTemplateAsync(Message message, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> GetTemplateAtVersionAsync(Message message, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> ValidateTemplateAsync(Message message, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> PublishTemplateAsync(Message message, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> Rollback(Message message, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> ListVersions(Message message, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Sends a message to the FCM service for delivery. The message gets validated both by
+        /// the Admin SDK, and the remote FCM service. A successful return value indicates
+        /// that the message has been successfully sent to FCM, where it has been accepted by the
+        /// FCM service.
+        /// <para>If the <paramref name="dryRun"/> option is set to true, the message will not be
+        /// actually sent to the recipients. Instead, the FCM service performs all the necessary
+        /// validations, and emulates the send operation. This is a good way to check if a
+        /// certain message will be accepted by FCM for delivery.</para>
+        /// </summary>
+        /// <returns>A task that completes with a message ID string, which represents
+        /// successful handoff to FCM.</returns>
+        /// <exception cref="ArgumentNullException">If the message argument is null.</exception>
+        /// <exception cref="ArgumentException">If the message contains any invalid
+        /// fields.</exception>
+        /// <exception cref="FirebaseMessagingException">If an error occurs while sending the
+        /// message.</exception>
+        /// <param name="message">The message to be sent. Must not be null.</param>
+        /// <param name="dryRun">A boolean indicating whether to perform a dry run (validation
+        /// only) of the send. If set to true, the message will be sent to the FCM backend service,
+        /// but it will not be delivered to any actual recipients.</param>
+        /// <param name="cancellationToken">A cancellation token to monitor the asynchronous
+        /// operation.</param>
+        public async Task<string> CreateTemplateFromJsonAsync(string json, bool dryRun, CancellationToken cancellationToken)
+        {
+            return await this.messagingClient.SendAsync(
+                message, dryRun, cancellationToken).ConfigureAwait(false);
         }
 
         /// <summary>
---
+++ FirebaseAdmin/FirebaseAdmin/RemoteConfig/RemoteConfigTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebaseAdmin.RemoteConfig
+{
+    /// <summary>
+    /// f.
+    /// </summary>
+    public class RemoteConfigTemplate
+    {
+        /// <summary>
+        /// f.
+        /// </summary>
+        public IEnumerable<RemoteConfigCondition> Conditions { get; set; }
+
+        public IDictionary<string, RemoteConfigParameter> Parameters { get; set; }
+
+        public IDictionary<string, RemoteConfigParameterGroup> ParameterGroups { get; set; }
+
+        public string ETag { get; }
+
+        public Version Version { get; set; }
+    }
+}
